a95114eb3020b0a1378daf9de319898450761373
DanielMota04 <dmota5705@gmail.com>
2025-08-26 11:46:57 -0300

    feat/tela de redefinir senha criada

--- FrontEnd-JotaNunes/src/App.jsx
+++ FrontEnd-JotaNunes/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route} from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import PasswordRecoveryPage from './pages/PasswordRecoveryPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
         <Route path='/register' element={<RegisterPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/password-recovery' element={<PasswordRecoveryPage/>}/>
+        <Route path='/reset-password' element={<ResetPasswordPage/>}/>
+
       </Routes>
     </div>
   );
---
+++ FrontEnd-JotaNunes/src/pages/ResetPasswordPage.jsx
@@ -0,0 +1,12 @@
+import ResetPasswordForm from '@/components/forms/ResetPassword/ResetPasswordForm';
+import styles from './ResetPasswordPage.module.css';
+
+const ResetPasswordPage = () => {
+  return (
+    <div className={styles.container}>
+      <ResetPasswordForm/>
+    </div>
+  );
+};
+
+export default ResetPasswordPage;
