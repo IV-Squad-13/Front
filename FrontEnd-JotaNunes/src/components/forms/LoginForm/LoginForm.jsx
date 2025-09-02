32f106a25840ef4436d74d1c3be8e88875055863
DanielMota04 <dmota5705@gmail.com>
2025-09-02 00:24:55 -0300

    testes feitos na tela de login

--- FrontEnd-JotaNunes/src/components/forms/LoginForm/LoginForm.jsx
+++ FrontEnd-JotaNunes/src/components/forms/LoginForm/LoginForm.jsx
@@ -1,29 +1,65 @@
 import styles from './LoginForm.module.css';
 import Logo from '@/assets/LogoPreta2.png';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Button from '@/components/button/Button';
 import Input from '@/components/input/Input';
+import { useState } from 'react';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError('');
+
+    try {
+      const response = await fetch('/mock/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || 'Falha no login');
+      }
+      console.log('Login bem-sucedido:', data.user);
+      navigate("/home")
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <div className={styles.formContainer}>
       <img src={Logo} alt="Logo da empresa Jotanunes" className={styles.logo} />
 
       <h1 className={styles.title}>Login</h1>
 
-      <form className={styles.form}>
-        <Input type="email" id="email" placeholder="E-mail" />
-        <Input type="password" id="password" placeholder="Password" />
-        <Link to="/home">
-          <Button type="submit">Entrar</Button>
-        </Link>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          id="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          id="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className={styles.error}>{error}</p>
+        
+        <Button type="submit">Entrar</Button>
       </form>
-
-      <p>
-        <Link className={styles.passwordRecovery} to="/password-recovery">
-          Esqueceu a senha?
-        </Link>
-      </p>
     </div>
   );
 };
