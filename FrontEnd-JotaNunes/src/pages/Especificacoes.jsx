b5e2744d675707823c85f83d376a9fb45d390627
DanielMota04 <dmota5705@gmail.com>
2025-09-23 18:30:03 -0300

    tela de historico finalizada

---
+++ FrontEnd-JotaNunes/src/pages/Especificacoes.jsx
@@ -0,0 +1,9 @@
+import styles from './Especificacoes.module.css'
+
+const Especificacoes = () => {
+  return (
+    <div className={styles.container}>Especificacoes</div>
+  )
+}
+
+export default Especificacoes
