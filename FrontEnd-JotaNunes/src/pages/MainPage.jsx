716456d4997aacbd237143f3a02dbf8deb33b936
DanielMota04 <dmota5705@gmail.com>
2025-10-13 22:50:44 -0300

    sidebar fechando

--- FrontEnd-JotaNunes/src/pages/MainPage.jsx
+++ FrontEnd-JotaNunes/src/pages/MainPage.jsx
@@ -2,14 +2,23 @@ import { Outlet } from 'react-router-dom';
 import styles from './MainPage.module.css';
 import SideBar from '@/components/sideBar/SideBar';
 import Header from '@/components/header/Header';
+import { useState } from 'react';
 
 const MainPage = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className={styles.container}>
-      <SideBar />
-      <div className={styles.mainContainer}>
-        <Header />
-        <Outlet /> 
+      <SideBar isOpen={isSidebarOpen} />
+      <div
+        className={`${styles.mainContainer} ${!isSidebarOpen ? styles.sidebarClosed : ''}`}
+      >
+        <Header toggleSidebar={toggleSidebar} />
+        <Outlet />
       </div>
     </div>
   );
