aca17e9262e2d29b9e60c3600e378b17853ef24c
Marcelo Gomes <marcelogomes@infox.com.br>
2025-11-19 10:27:30 -0300

    Dashboard agora contem contagem dos itens no catálogo.

--- FrontEnd-JotaNunes/src/pages/Dashboard/Dashboard.jsx
+++ FrontEnd-JotaNunes/src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,31 @@
-import styles from './Dashboard.module.css'
-import ItemCard from '@/components/ItemCard/ItemCard'
+import styles from './Dashboard.module.css';
+import CatalogCount from '@/components/ContadorDeItens/contadorDeItem'; 
+import React from 'react';
+
+const specs = [
+  { type: "padrao", name: "Padrões" },
+  { type: "ambiente", name: "Ambientes" },
+  { type: "item", name: "Itens" },
+  { type: "material", name: "Materiais" },
+  { type: "marca", name: "Marcas" },
+];
 
 const Dashboard = () => {
+
   return (
     <div>
-      <div className={styles.container}>Dashboard</div>
+      <div className={styles.container}><h2>Dashboard do Catálogo</h2></div>
       
-      <div>
-        <ItemCard/>
-        <ItemCard/>
-        <ItemCard/>
+      <div className={styles.cardGrid}>
+        
+        {specs.map((spec) => (
+          <CatalogCount
+            key={spec.type} 
+            resourceType={spec.type}
+            displayName={spec.name}   
+          />
+        ))}
+        
       </div>
     </div>
   )
